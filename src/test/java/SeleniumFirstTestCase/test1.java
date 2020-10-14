dd004e29cb2801af9763c22c2efd7e5d7450cf5b
Prabu <mails2prabuselvan15@gmail.com>
2020-10-14 20:51:22 +0530

    parallel execution through testng.xml

--- src/test/java/SeleniumFirstTestCase/test1.java
+++ src/test/java/SeleniumFirstTestCase/test1.java
@@ -1,7 +1,10 @@
 package SeleniumFirstTestCase;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -9,24 +12,58 @@ import org.testng.annotations.Test;
 
 public class test1 {
 	
-	private WebDriver driver;
+	public WebDriver driver;
 
 	@BeforeTest
 	public void beforeTest() {
-		System.out.println("Chrome driver is initiated");
+//		System.out.println("Chrome driver is initiated");
 		System.setProperty("webdriver.chrome.driver", "/Users/prabuselvan/Downloads/chromedriver 5");
 		driver = new ChromeDriver();
+		
+//		System.setProperty("webdriver.gecko.driver", "/Users/prabuselvan/Downloads/geckodriver");
+//		driver = new FirefoxDriver();
+	}
+	
+//	@Test
+//	public void ChromeCase() {
+//
+//	
+//		driver.get("https://testautomationu.applitools.com/");
+//		System.out.println("Launched the browser");
+//		String title = driver.getTitle();
+//		Assert.assertEquals("Test Automation University | Applitools",title);
+//	}
+	
+//	@Test
+//	public void Test1() {
+//
+//		driver.get("https://testautomationu.applitools.com/");
+//		System.out.println("Launched the browser");
+//		String title = driver.getTitle();
+//		Assert.assertEquals("Test Automation University | Applitools",title);
+//		
+//	}
+	
+	@Test
+	public void Test1() {
+		driver.get("https://www.google.com");
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+		Assert.assertEquals(driver.getTitle(), "Google");
 	}
 	
 	@Test
-	public void firstTestCase() {
-	
-		driver.get("https://testautomationu.applitools.com/");
-		System.out.println("Launched the browser");
-		String title = driver.getTitle();
-		Assert.assertEquals("Test Automation University | Applitools",title);
+	public void Test2() {
+		driver.get("https://www.amazon.in/");
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+		Assert.assertEquals(driver.getTitle(), "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in");
 	}
 	
+	@Test
+	public void Test3() {
+		driver.get("https://www.flipkart.com");
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+		Assert.assertEquals(driver.getTitle(),"Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!");
+	}
 	@AfterTest
 	public void afterTest() {
 		driver.quit();
